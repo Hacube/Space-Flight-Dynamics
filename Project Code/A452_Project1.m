5283154b730e3f9bf358eae3112e0f23d7d9326b
Jacob Amezquita <113970708+Hacube@users.noreply.github.com>
2024-10-10 14:34:53 -0700

    First Commit
    Need to complete initial starting parameters.

---
+++ Project Code/A452_Project1.m
@@ -0,0 +1,20 @@
+%% A452 Project 1 Rendezvous
+% Jacob Amezquita
+% Ryan Pettigrew
+
+% S/C A
+ecc_A = 0.0002046; 
+inc_A = 0.0921;    % degs
+zp_A = 35777;      % km 
+za_A = 35794;      % km 
+raan_A = 79.9111;  % degs 
+omega_A = 98.1606; % degs 
+T_A = 1.00272835;  % rev/day
+ma_A = 210.4111;   % degs, mean anomaly
+
+
+% S/C B
+
+
+
+
